b85d5634126facd380c4667a64ca078e5cdf63ce
Jesse Nagelberg <jnagelberg@gmail.com>
2015-03-26 13:57:27 -0400

    added some layout and youtube

--- app/assets/javascripts/spaces.js
+++ app/assets/javascripts/spaces.js
@@ -1,35 +1,32 @@
-// var map;
+var map;
+var service;
 
-// function renderMap(location) {
-// 	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
-// 	var mapOptions = {
-// 		center: currentLocation,
-// 		zoom: 14,
-// 		mapTypeId: google.maps.MapTypeId.ROADMAP
-// 	};
 
-// 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+function handleSearchResults(results, status){
+	console.log(results);
 
-// 	var marker = new google.maps.Marker({
-// 		position: currentLocation,
-// 		map: map,
-// 		animation: google.maps.Animation.DROP,
-// 		icon: 'http://i.imgur.com/MrPOkeX.png?1'
-// 	});
-// }
+	for(var i =0; i < results.length; i++){
+		var marker = new google.maps.Marker({
+			position: results[i].geometry.location,
+			map: map,
+			icon: "http://i.imgur.com/XzDJvi2.png?2"
+		})
+	}
+}
 
-// function toggleBounce(){
-// 	if (marker.getAnimation() !== null) {
-// 		market.setAnimation(null);
-// 	} else {
-// 		marker.setAnimation(google.maps.Animation.BOUNCE);
-// 	}
-// }
+function performSearch(){
+	var request = {
+		bounds: map.getBounds(),
+		name: "Chipotle Mexican Grill"
+	}
 
-function renderMap(location){
-	console.log(location);
+	service.nearbySearch(request, handleSearchResults);
+}
 
-	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+function renderMap(position){
+	console.log(position);
+
+	var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 	var mapOptions = {
 		center: currentLocation,
 		zoom: 14,
@@ -38,6 +35,29 @@ function renderMap(location){
 
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+	var marker = new google.maps.Marker({
+		position: currentLocation,
+		map: map
+	});
+
+	service = new google.maps.places.PlacesService(map);
+
+	// This ensures that the map bounds have been initialized before we begin the search.
+	google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+
+	// Circles are fun...
+	var circleOptions = {
+		strokeColor: "#0000FF",
+		strokeOpacity: 0.8,
+		strokeWeight: 1.5,
+		fillColor: "#0000FF",
+		fillOpacity: 0.35,
+		map: map,
+		center: currentLocation,
+		radius: 300
+	};
+
+	var circle = new google.maps.Circle(circleOptions);
 }
 
 
