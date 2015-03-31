0a45ce9c7818b6a6543cf7e9dbac88f445744aef
Jesse Nagelberg <jnagelberg@gmail.com>
2015-03-31 16:45:27 -0400

    added some styling, smooth scrolling, and footer

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,3 +1,11 @@
 $(document).ready(function (){
 	$(".button-collapse").sideNav();
 });
+
+// Smooth Scrolling
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 400);
+    return false;
+});
