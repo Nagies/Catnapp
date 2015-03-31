a6113b9478a20b400ef0ea9fdf55460e30bd2a33
Jesse Nagelberg <jnagelberg@gmail.com>
2015-03-31 12:39:27 -0400

    fixed up form

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,8 +1,6 @@
 var map;
-var service;
 
-
-function initialize(){
+function loadSpaces(){
 	// console.log(results);
 
 	$.ajax({
@@ -17,23 +15,42 @@ function initialize(){
 					map: map,
 					icon: "http://i.imgur.com/XzDJvi2.png?2"
 				})
-
-				google.maps.event.addListener(marker, 'click', function() {
-					var $space_img = $('<img>').attr({'src': result.image, width: '100%'}).addClass('m4 col');
-					var $space_info = $('<dl>').addClass('m8 col').css("color", "white");
-					$space_info.append($('<dt>').text('Email')).append($('<dd>').text(result.email));
-					$space_info.append($('<dt>').text('Address')).append($('<dd>').text(result.address));
-					$space_info.append($('<dt>').text('Description')).append($('<dd>').text(result.description));
-					$space_info.append($('<dt>').text('Rate')).append($('<dd>').text(result.rate));
-
-
-					$('#space_info').empty();
-					$('#space_info').append($space_img);
-					$('#space_info').append($space_info);	
-
-				})
+				clickEvent(result, marker);
 			})
 		}
+	})
+}
+
+function createSpace(event){
+	var newSpaceForm = $('form#new_space')
+	if (event){ event.preventDefault(); }
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode ({ 'address' : newSpaceForm.find('#space_address').val()}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			newSpaceForm.find('#space_lat').val(results[0].geometry.location.lat());
+			newSpaceForm.find('#space_lng').val(results[0].geometry.location.lng());
+			$.ajax({
+				url: '/spaces',
+				type: 'POST',
+				dataType: 'json',
+				data: newSpaceForm.serialize(),
+				success: function (space) {
+					console.log(space);
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(space.lat, space.lng),
+						map: map,
+						icon: "http://i.imgur.com/XzDJvi2.png?2"
+					})
+
+					clickEvent(space, marker);
+				}
+			})
+		} else {
+			// Error condition to do...
+
+			
+		}
+
 	})
 }
 
@@ -55,7 +72,8 @@ function renderMap(position){
 	var mapOptions = {
 		center: currentLocation,
 		zoom: 14,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false
 	};
 
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -65,10 +83,10 @@ function renderMap(position){
 		map: map
 	});
 
-	service = new google.maps.places.PlacesService(map);
+	// service = new google.maps.places.PlacesService(map);
 
 	// This ensures that the map bounds have been initialized before we begin the search.
-	google.maps.event.addListenerOnce(map, 'bounds_changed', initialize);
+	google.maps.event.addListenerOnce(map, 'bounds_changed', loadSpaces);
 
 	// Circles are fun...
 	var circleOptions = {
@@ -114,9 +132,28 @@ function renderMap(position){
 // 	});
 // }
 
+function clickEvent(result, marker) {
+
+	google.maps.event.addListener(marker, 'click', function() {
+		var $space_img = $('<img>').attr({'src': result.image, width: '100%'}).addClass('m4 col');
+		var $space_info = $('<dl>').addClass('m8 col').css("color", "white");
+		$space_info.append($('<dt>').text('Email')).append($('<dd>').text(result.email));
+		$space_info.append($('<dt>').text('Address')).append($('<dd>').text(result.address));
+		$space_info.append($('<dt>').text('Description')).append($('<dd>').text(result.description));
+		$space_info.append($('<dt>').text('Rate')).append($('<dd>').text(result.rate));
+		$('#space_info').empty();
+		$('#space_info').append($space_img);
+		$('#space_info').append($space_info);	
+
+	})
+
+}
+
+
 
 $(document).ready(function (){
 	navigator.geolocation.getCurrentPosition(renderMap);
+	$('form#new_space').on('submit', createSpace);
 });
 
 
--- app/views/spaces/show.json.jbuilder
+++ app/views/spaces/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @space, :id, :address, :rate, :description, :user_id, :created_at, :updated_at
+json.extract! @space, :id, :address, :rate, :description, :user_id, :created_at, :updated_at, :lat, :lng, :image
