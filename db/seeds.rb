3f722afca14bbaf6cd3510a5f65d159152cc5817
Jesse Nagelberg <jnagelberg@gmail.com>
2015-03-31 14:50:35 -0400

    cleaning up code and refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-Space.create!({address: "536 Avenue of the Americas
-New York, NY 10011", rate: 25, description: "Bacon ipsum dolor amet meatball pig pork chop picanha cow. Tail bresaola picanha ham hock pancetta beef ribs pork loin andouille flank leberkas tri-tip landjaeger short ribs spare ribs tenderloin. Jowl capicola fatback, filet mignon landjaeger brisket biltong meatball cow. Pork belly kevin meatloaf, spare ribs tongue capicola jowl shoulder chuck tenderloin bacon jerky. Andouille chicken hamburger picanha tongue, ball tip brisket. Pastrami turkey short ribs alcatra filet mignon capicola pig boudin pork belly short loin sirloin.", image: "http://cdn-img0.streeteasy.com/nyc/image/12/111631612.jpg", lat: 40.738541, lng: -73.996604, email: "kirsten.greenholt@corkeryfisher.info"})
+Space.create!({
+	address: "536 Avenue of the Americas New York, NY 10011", 
+	rate: 25, 
+	description: "Bacon ipsum dolor amet meatball pig pork chop picanha cow. Tail bresaola picanha ham hock pancetta beef ribs pork loin andouille flank leberkas tri-tip landjaeger short ribs spare ribs tenderloin. Jowl capicola fatback, filet mignon landjaeger brisket biltong meatball cow. Pork belly kevin meatloaf, spare ribs tongue capicola jowl shoulder chuck tenderloin bacon jerky. Andouille chicken hamburger picanha tongue, ball tip brisket. Pastrami turkey short ribs alcatra filet mignon capicola pig boudin pork belly short loin sirloin.", 
+	image: "http://cdn-img0.streeteasy.com/nyc/image/12/111631612.jpg", 
+	lat: 40.738541, 
+	lng: -73.996604, 
+	email: "kirsten.greenholt@corkeryfisher.info"})
 
